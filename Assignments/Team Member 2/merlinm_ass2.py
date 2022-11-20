155f458d44722c8d99a68e15254fc413c2051237
ambiaravinthpersonal <81503698+ambiaravinthpersonal@users.noreply.github.com>
2022-11-20 18:21:02 +0530

    Delete Assignments directory

--- Assignments/Team Member 2/merlinm_ass2.py
+++
@@ -1,7 +0,0 @@
-import random
-temp=random.randint(0,100)
-hum=random.randint(0,100)
-print("Temperature= ",temp)
-print("Humidity= ",hum)
-if(temp>50):
-    print("Alert----------->: Now Temperature is ",temp)
